4e17c5086c981913729092ab97f2aaff2b61d3b8
Daniel Neto <me@danielneto.com>
2025-03-24 16:12:34 -0300

    Update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { execSync } = require("child_process");
 const { getPluginData } = require("./mysql");
 const MessageHandler = require("./MessageHandler");
 
-const thisServerVersion = '5';
+const thisServerVersion = '6';
 var serverVersion = '0';
 var phpSocketDataObj = {};
 
