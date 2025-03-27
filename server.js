ebaea7176ee8c7d3dd5b1d80cc52a15a208484b1
Daniel Neto <me@danielneto.com>
2025-03-27 14:13:05 -0300

    Update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const MessageHandler = require("./MessageHandler");
 const logger = require('./logger');
 const serverStartTime = Date.now();
 
-const thisServerVersion = '37';
+const thisServerVersion = '38';
 let serverVersion = '0';
 let phpSocketDataObj = {};
 
